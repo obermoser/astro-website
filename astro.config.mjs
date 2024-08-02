782bb79c1fd32258dca70b6b21deafdcd8696907
Bernhard Obermoser <bernhard@MacBook-Pro-von-Bernhard.local>
2024-08-02 23:10:53 +0200

    Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sentry(), spotlightjs()]
+  integrations: [
+    sentry(),
+    spotlightjs(), partytown({
+    config:{debug:false}
+  })]
 });
