9b6be300304f4cef3bd674700270e475cf1f874c
Bernhard Obermoser <bernhard@MacBook-Pro-von-Bernhard.local>
2024-08-02 23:32:24 +0200

    Base Layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,4 @@
 import { defineConfig } from 'astro/config';
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
-
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sentry(),
-    spotlightjs(), partytown({
-    config:{debug:true}
-  })]
 });
