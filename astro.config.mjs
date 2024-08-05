8bf54f20a5ad82167b19de7cc7d8be7702755f07
Bernhard <bernhard.obermoser@icloud.com>
2024-08-05 14:18:12 +0200

    Transitions & Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://obermoser.pages.dev",
-  integrations: [preact()]
+  integrations: [preact(), sitemap()]
 });
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,6 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
+document.addEventListener('astro:page-load', () => {
+
+  document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
   });
+})
