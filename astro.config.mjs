4e63960d5a5572138e3ce84fd0e995d23da86724
Bernhard <bernhard.obermoser@icloud.com>
2024-08-05 16:03:59 +0200

    Content Collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
-
 import sitemap from "@astrojs/sitemap";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://obermoser.pages.dev",
-  integrations: [preact(), sitemap()]
+  integrations: [preact(), sitemap(), sentry(), spotlightjs()]
 });
